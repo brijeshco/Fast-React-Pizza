9d5f2ae7b55a538e6c7d28b82eca718f2406828d
Brijesh <forndline@protonmail.com>
2023-10-20 19:13:46 +0530

    error

--- src/Features/menu/MenuItem.jsx
+++ src/Features/menu/MenuItem.jsx
@@ -1,9 +1,9 @@
-import { useDispatch, useSelector } from "react-redux";
-import Button from "../../ui/Button";
-import { formatCurrency } from "../../utils/helpers";
-import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
-import DeleteItem from "../cart/DeleteItem";
-import UpdateItemQuantity from "../cart/updateItemQuantity";
+import { useDispatch, useSelector } from 'react-redux';
+import Button from '../../ui/Button';
+import { formatCurrency } from '../../utils/helpers';
+import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
+import DeleteItem from '../cart/DeleteItem';
+import UpdateItemQuantity from '../cart/updateItemQuantity';
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
@@ -25,37 +25,37 @@ function MenuItem({ pizza }) {
   }
 
   return (
-    <li className="flex gap-4 py-2">
+    <li className='flex gap-4 py-2'>
       <img
         src={imageUrl}
         alt={name}
-        className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`}
+        className={`h-24 ${soldOut ? 'opacity-70 grayscale' : ''}`}
       />
-      <div className="flex grow flex-col pt-0.5">
-        <p className="font-medium">{name}</p>
-        <p className="text-sm capitalize italic text-stone-500">
-          {ingredients.join(", ")}
+      <div className='flex grow flex-col pt-0.5'>
+        <p className='font-medium'>{name}</p>
+        <p className='text-sm capitalize italic text-stone-500'>
+          {ingredients.join(', ')}
         </p>
-        <div className="mt-auto flex items-center justify-between">
+        <div className='mt-auto flex items-center justify-between'>
           {!soldOut ? (
-            <p className="text-sm">{formatCurrency(unitPrice)}</p>
+            <p className='text-sm'>{formatCurrency(unitPrice)}</p>
           ) : (
-            <p className="tex-stone-500 text-sm font-medium uppercase">
+            <p className='tex-stone-500 text-sm font-medium uppercase'>
               Sold out
             </p>
           )}
           {isInCart && (
-            <div className="flex items-center gap-5 sm:gap-8">
+            <div className='flex items-center gap-5 sm:gap-8'>
               <UpdateItemQuantity
                 pizzaId={id}
                 currentQuantity={currentQuantity}
               />
-              <DeleteItem pizzaId={id} />{" "}
+              <DeleteItem pizzaId={id} />{' '}
             </div>
           )}
 
           {!soldOut && !isInCart && (
-            <Button type="small" onClick={handleAddCart}>
+            <Button type='small' onClick={handleAddCart}>
               Add to cart
             </Button>
           )}
