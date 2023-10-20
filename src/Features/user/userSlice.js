4f51d41764ca5de447e1b91cbed4d8b943ca0abd
Brijesh <forndline@protonmail.com>
2023-10-20 20:32:08 +0530

    link

--- src/Features/user/userSlice.js
+++ src/Features/user/userSlice.js
@@ -10,19 +10,15 @@ function getPosition() {
 export const fetchAddress = createAsyncThunk(
   'user/fetchAddress',
   async function () {
-    // 1) We get the user's geolocation position
     const positionObj = await getPosition();
     const position = {
       latitude: positionObj.coords.latitude,
       longitude: positionObj.coords.longitude,
     };
-
-    // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
+    g;
     const addressObj = await getAddress(position);
     const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
 
-    // 3) Then we return an object with the data that we are interested in.
-    // Payload of the FULFILLED state
     return { position, address };
   }
 );
@@ -45,7 +41,7 @@ const userSlice = createSlice({
   },
   extraReducers: (builder) =>
     builder
-      .addCase(fetchAddress.pending, (state, action) => {
+      .addCase(fetchAddress.pending, (state) => {
         state.status = 'loading';
       })
       .addCase(fetchAddress.fulfilled, (state, action) => {
@@ -53,7 +49,7 @@ const userSlice = createSlice({
         state.address = action.payload.address;
         state.status = 'idle';
       })
-      .addCase(fetchAddress.rejected, (state, action) => {
+      .addCase(fetchAddress.rejected, (state) => {
         state.status = 'error';
         state.error =
           'There was a problem getting your address. Make sure to fill this field!';
